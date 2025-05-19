884a7b88763327436131defd36966ee97e02236d
Oleksandr Soloviov <shakal194@gmail.com>
2025-05-19 10:30:12 +0300

    ашч slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,29 @@
-const profitSwiper = new Swiper('.profit-swiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    autoplay: {
-        delay: 1500,
-    },
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination-custom',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        }
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        const profitSwiper = new Swiper('.profit-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 1500,
+                disableOnInteraction: false,
+            },
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination-custom',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                }
+            }
+        });
+    }, 100);
 }); 
